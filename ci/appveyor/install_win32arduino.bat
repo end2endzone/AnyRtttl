eaf266a89114a124539237151a7666d6769c5048
Antoine Beauchamp <end2endzone@gmail.com>
2023-03-12 14:52:12 -0400

    - Integrated GitHub Actions to the project. - Refactored CI build script for reusability and easier maintenance.

--- ci/appveyor/install_win32arduino.bat
+++ ci/appveyor/install_win32arduino.bat
@@ -6,37 +6,6 @@ if "%APPVEYOR_BUILD_FOLDER%"=="" (
   exit /B 1
 )
 
-set GTEST_ROOT=%APPVEYOR_BUILD_FOLDER%\third_parties\googletest\install
-set rapidassist_DIR=%APPVEYOR_BUILD_FOLDER%\third_parties\RapidAssist\install
-set win32arduino_DIR=%APPVEYOR_BUILD_FOLDER%\third_parties\win32Arduino\install
-
-echo ============================================================================
-echo Cloning win32Arduino into %APPVEYOR_BUILD_FOLDER%\third_parties\win32Arduino
-echo ============================================================================
-mkdir %APPVEYOR_BUILD_FOLDER%\third_parties >NUL 2>NUL
-cd %APPVEYOR_BUILD_FOLDER%\third_parties
-git clone "https://github.com/end2endzone/win32Arduino.git"
-cd win32Arduino
-echo.
-
-echo Checking out version 2.3.1...
-git checkout 2.3.1
-echo.
-
-echo ============================================================================
-echo Compiling...
-echo ============================================================================
-mkdir build >NUL 2>NUL
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=%win32arduino_DIR% ..
+:: Call matching script for windows
+call "%APPVEYOR_BUILD_FOLDER%\ci\windows\%~n0.bat"
 if %errorlevel% neq 0 exit /b %errorlevel%
-cmake --build . --config %Configuration%
-if %errorlevel% neq 0 exit /b %errorlevel%
-echo.
-
-echo ============================================================================
-echo Installing into %win32arduino_DIR%
-echo ============================================================================
-cmake --build . --config %Configuration% --target INSTALL
-if %errorlevel% neq 0 exit /b %errorlevel%
-echo.
